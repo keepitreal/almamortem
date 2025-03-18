aa806e141546ca4ed81113ced5a67318d4b27085
Myk <89474773+mykcryptodev@users.noreply.github.com>
2025-03-18 19:07:18 -0400

    rewrite well known path (#29)

--- next.config.js
+++ next.config.js
@@ -30,6 +30,14 @@ const config = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: '/.well-known/farcaster.json',
+        destination: '/api/.well-known/farcaster.json',
+      },
+    ];
+  },
 };
 
 export default config;
